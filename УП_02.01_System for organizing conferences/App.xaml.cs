e78903bee8ed2779edaa7a619834fa7959a4b7af
Sidakov21 <amirsidacov09@gmail.com>
2026-01-30 13:59:51 +0300

    Добавьте файлы проекта.

---
+++ УП_02.01_System for organizing conferences/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace УП_02._01_System_for_organizing_conferences
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
